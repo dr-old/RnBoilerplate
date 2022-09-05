6ad326f64e6bbe4317c14bc20d5895a880e6812f
dr-old <danniramdan96@gmail.com>
2022-09-05 09:45:56 +0700

    ADD BUTTON ICON STYLE

--- src/containers/pages/Home/index.js
+++ src/containers/pages/Home/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {View, Text, Button} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
-import {ButtonIcon, ButtonLabel} from '../../../components/atoms';
+import {ButtonIcon, ButtonLabel, Divider} from '../../../components/atoms';
 import {color} from '../../../utils/styles';
 
 function Home() {
@@ -24,12 +24,79 @@ function Home() {
           flexWrap: 'wrap',
         }}>
         <ButtonIcon
+          type="primary"
+          name="home"
+          size={20}
+          onClick={() => console.log('primary')}
+        />
+        <ButtonIcon
+          type="success"
+          name="home"
+          size={20}
+          onClick={() => console.log('success')}
+        />
+        <ButtonIcon
+          type="warning"
+          name="home"
+          size={20}
+          onClick={() => console.log('warning')}
+        />
+        <ButtonIcon
+          type="default"
+          name="home"
+          size={20}
+          onClick={() => console.log('default')}
+        />
+        <ButtonIcon
+          type="danger"
+          name="home"
+          size={20}
+          onClick={() => console.log('danger')}
+        />
+      </View>
+      <Text>Button Icon Solid</Text>
+      <View
+        style={{
+          width: '100%',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          paddingHorizontal: 17,
+          flexWrap: 'wrap',
+        }}>
+        <ButtonIcon
           solid={true}
           type="primary"
           name="home"
           size={20}
-          colorIcon={color.blue}
           onClick={() => console.log('primary')}
+        />
+        <ButtonIcon
+          solid={true}
+          type="success"
+          name="home"
+          size={20}
+          onClick={() => console.log('success')}
+        />
+        <ButtonIcon
+          solid={true}
+          type="warning"
+          name="home"
+          size={20}
+          onClick={() => console.log('warning')}
+        />
+        <ButtonIcon
+          solid={true}
+          type="default"
+          name="home"
+          size={20}
+          onClick={() => console.log('default')}
+        />
+        <ButtonIcon
+          solid={true}
+          type="danger"
+          name="home"
+          size={20}
+          onClick={() => console.log('danger')}
         />
       </View>
       <Text>Button Label</Text>
@@ -107,10 +174,12 @@ function Home() {
           onClick={() => console.log('danger')}
         />
       </View>
-
-      <Button
-        title="Go to Typography"
-        onPress={() => navigation.navigate('Typography')}
+      <Divider height={10} mTop={10} mBot={10} bgColor={color.white2} />
+      <ButtonLabel
+        type="primary"
+        solid={true}
+        label="go to typography"
+        onClick={() => navigation.navigate('Typography')}
       />
     </View>
   );
